8f2dc29d342b17c6dbb2eb25c71b82b6e2a3b9b5
Mochamad Fajar Sodik <mochamad.fajarsodik@gmail.com>
2025-04-26 22:55:05 +0700

    finish add bootstrap

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -17,27 +17,44 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
 
   final appBuilder = await builder();
 
-  // Bloc.observer =
+  Bloc.observer = MyBlocObserver();
+  runApp(appBuilder);
 }
 
 class MyBlocObserver extends BlocObserver {
   @override
+  void onCreate(BlocBase bloc) {
+    super.onCreate(bloc);
+    debugPrint('onCreate --> ${bloc.runtimeType}');
+  }
+
+  @override
   void onEvent(Bloc bloc, Object? event) {
     super.onEvent(bloc, event);
+    debugPrint('onEvent --> ${bloc.runtimeType}, $event');
   }
 
   @override
   void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
     super.onError(bloc, error, stackTrace);
+    debugPrint('onError --> ${bloc.runtimeType}, $error');
   }
 
   @override
   void onChange(BlocBase bloc, Change change) {
     super.onChange(bloc, change);
+    debugPrint('onChange --> ${bloc.runtimeType}, $change');
   }
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
+    debugPrint('onTransition --> ${bloc.runtimeType}, $transition');
+  }
+
+  @override
+  void onClose(BlocBase bloc) {
+    super.onClose(bloc);
+    debugPrint('onClose --> ${bloc.runtimeType}');
   }
 }
